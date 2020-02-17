dc0cc487d8cc64ebf89267445bc51e9132de0ffc
Kristina Korneeva <kristinaland23@gmail.com>
2020-02-17 18:28:43 +0300

    Добавляет открытие/закрытие окна. Меняет цвет мантии, глаз, фаербола по клику

--- js/setup.js
+++ js/setup.js
@@ -4,17 +4,39 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_MANTLE_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYE_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARDS_FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
-var userDialog = document.querySelector('.setup');
+var ESC_KEY = 'Escape';
+var ENTER_KEY = 'Enter';
+var MIN_NAME_LENGTH = 2;
+var MAX_NAME_LENGTH = 25;
+
+// переменные
+var setup = document.querySelector('.setup');
 var similarCharacters = document.querySelector('.setup-similar');
 var wizardsNode = document.querySelector('.setup-similar-list');
+
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+var userNameInput = setup.querySelector('.setup-user-name');
+
+var setupPlayer = document.querySelector('.setup-player');
+var setupWizard = setupPlayer.querySelector('.setup-wizard');
+var wizardCoat = setupWizard.querySelector('.wizard-coat');
+var coatInput = setupPlayer.querySelector('input[name="coat-color"]');
+var wizardEyes = setupWizard.querySelector('.wizard-eyes');
+var eyesInput = setupPlayer.querySelector('input[name="eyes-color"]');
+var wizardFireball = setupPlayer.querySelector('.setup-fireball-wrap');
+var fireballInput = setupPlayer.querySelector('input[name="fireball-color"]');
+
+
 // шаблон, который будем копировать
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
   .content
   .querySelector('.setup-similar-item');
 
 // удаляет класс, скрывающий окно
-userDialog.classList.remove('hidden');
+setup.classList.remove('hidden');
 
 // функция рандомное число
 var getRandomNumber = function (min, max) {
@@ -59,3 +81,75 @@ renderWizards();
 
 // удаляет класс, скрывающий окно c похожими персонажами
 similarCharacters.classList.remove('hidden');
+
+
+// Открытие/закрытие окна настройки персонажа
+var onPopupEscPress = function (evt) {
+  if (evt.key === ESC_KEY) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.key === ENTER_KEY) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.key === ENTER_KEY) {
+    closePopup();
+  }
+});
+
+// сообщение об ошибке
+var inputInvalidHandler = function (evt) {
+  var target = evt.target;
+  if (target.value.length < MIN_NAME_LENGTH) {
+    userNameInput.setCustomValidity('Имя должно состоять минимум из ' + MIN_NAME_LENGTH + '-х символов');
+  } else if (target.value.length > MAX_NAME_LENGTH) {
+    userNameInput.setCustomValidity('Имя не должно превышать ' + MAX_NAME_LENGTH + '-ти символов');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+};
+userNameInput.addEventListener('invalid', inputInvalidHandler);
+
+// изменение цвета
+wizardCoat.addEventListener('click', function () {
+  var color = getRandomArrayElem(WIZARD_MANTLE_COLORS);
+  wizardCoat.style.fill = color;
+  coatInput.value = color;
+});
+
+wizardEyes.addEventListener('click', function () {
+  var color = getRandomArrayElem(WIZARD_EYE_COLORS);
+  wizardEyes.style.fill = color;
+  eyesInput.value = color;
+});
+
+wizardFireball.addEventListener('click', function () {
+  var color = getRandomArrayElem(WIZARDS_FIREBALL_COLORS);
+  wizardFireball.style.background = color;
+  fireballInput.value = color;
+});
+
+
